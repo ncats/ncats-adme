9d233b40a59670024cbe98827fa1f4e440161fba
Jorge Neyra <jorge.neyra@nih.gov>
2020-07-23 12:36:43 -0400

    added download data and link

--- client/src/app/method/method.component.ts
+++ client/src/app/method/method.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'adme-method',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./method.component.scss']
 })
 export class MethodComponent implements OnInit {
+  dataDownloadHref: SafeUrl;
 
-  constructor() { }
+  constructor(
+    private domSanatizer: DomSanitizer
+  ) {
+    this.dataDownloadHref = domSanatizer.bypassSecurityTrustResourceUrl(`${environment.baseHref}assets/rlm_public_set.xlsx`);
+  }
 
   ngOnInit(): void {
   }
