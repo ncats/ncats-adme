f2d0bc84112e2dd47c39308e48a3a80e9d723fbf
mapleknight <mapleknight@users.noreply.github.com>
2020-11-20 20:40:55 -0500

    Update startup.sh

--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-//gunicorn --workers=2 --threads=4 --worker-class=gthread --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
-python app.py
+gunicorn --workers=2 --threads=4 --worker-class=gthread --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
