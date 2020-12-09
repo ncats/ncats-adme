7df62640418bfba370cd3ad1458c98890b4c55fc
Jorge Neyra <jorge.neyra@nih.gov>
2020-12-09 14:48:05 -0500

    updated script call again to 4 workers

--- server/startup.sh
+++ server/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --workers=2 --threads=2 --worker-class=sync --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
+gunicorn --workers=4 --worker-class=sync --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
