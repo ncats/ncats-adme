4d6e9632d410cbcfe2acdd3592e475fae7e81e84
Jorge Neyra <jorge.neyra@nih.gov>
2020-12-09 14:38:51 -0500

    updated startup script

--- server/startup.sh
+++ server/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --workers=1 --threads=1 --worker-class=gthread --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
+gunicorn --workers=2 --threads=2 --worker-class=sync --worker-tmp-dir /dev/shm --log-file=- --bind 0.0.0.0:5000 wsgi:app --timeout 600
