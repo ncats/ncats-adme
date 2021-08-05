fe210e669a4d7be7a6748ac4abb5428c93018e07
Jorge Neyra <jorge.neyra@nih.gov>
2021-08-05 11:09:54 -0400

    added code to track predictions

--- server/predictors/base/base.py
+++ server/predictors/base/base.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 class PredictorBase:
 
@@ -9,6 +10,7 @@ class PredictorBase:
         self.predictions_df = pd.DataFrame()
         self.has_errors = False
         self.model_errors = []
+        self.raw_predictions_df = pd.DataFrame()
 
     def get_errors(self):
         return {
@@ -17,3 +19,10 @@ class PredictorBase:
 
     def columns_dict(self):
         return self._columns_dict.copy()
+
+    def record_predictions(self, file_path):
+        if len(self.raw_predictions_df.index) > 0:
+            with open(file_path, 'a') as fw:
+                rows = self.raw_predictions_df.values.tolist()
+                cw = csv.writer(fw)
+                cw.writerows(rows)
