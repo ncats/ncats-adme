d7e81080d356fa7af4e1eb0e477602d7e1baf043
iwwwish <siramshettyv2@nih.gov>
2022-01-13 13:33:46 -0500

    added global footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ResolveEnd, Router, RouterEvent } from '@angular/router';
@@ -6,6 +7,7 @@ import { Subscription } from 'rxjs';
 import { GoogleAnalyticsService } from './google-analytics/google-analytics.service';
 import { DOCUMENT } from '@angular/common';
 import { DEPLOY_URL } from './utilities/deploy-url';
+import * as moment from 'moment';
 
 @Component({
   selector: 'adme-root',
@@ -14,11 +16,16 @@ import { DEPLOY_URL } from './utilities/deploy-url';
 })
 export class AppComponent implements OnInit, OnDestroy {
   private routerSubscription: Subscription;
+  data: any = [];
+  release_date: string;
+  release_url: string;
+  tag_name: string;
   constructor(
     iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer,
     private router: Router,
     private gaService: GoogleAnalyticsService,
+    private http: HttpClient,
     // tslint:disable-next-line:variable-name
     @Inject(DOCUMENT) private _document: HTMLDocument,
     @Inject(DEPLOY_URL) private deployUrl: string
@@ -27,6 +34,15 @@ export class AppComponent implements OnInit, OnDestroy {
       'cancel',
       sanitizer.bypassSecurityTrustResourceUrl(`${deployUrl}assets/icons/cancel-24px.svg`));
   }
+  getReleaseData(){
+   const url ='https://api.github.com/repos/ncats/ncats-adme/releases/latest'
+   this.http.get(url).subscribe((res)=>{
+     this.data = res
+     this.release_date = moment.utc(this.data.published_at).local().format('DD/MM/YYYY');
+     this.release_url = this.data.html_url
+     this.tag_name = this.data.tag_name
+   })
+  }
   ngOnInit() {
     this._document.getElementById('appFavicon').setAttribute('href', `${this.deployUrl}assets/icons/favicon.ico`);
     this.routerSubscription = this.router.events.subscribe((event: RouterEvent) => {
@@ -34,6 +50,7 @@ export class AppComponent implements OnInit, OnDestroy {
         this.gaService.sendPageView(event.state.root.firstChild.data.pageTitle, event.state.url);
       }
     });
+    this.getReleaseData();
   }
   ngOnDestroy() {
     if (this.routerSubscription != null) {
