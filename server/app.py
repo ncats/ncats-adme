5111bf9fa99475e9a44237b54fc93f63e7a6e33d
iwwwish <siramshettyv2@nih.gov>
2022-05-23 15:21:43 -0400

    implemented row limit check for similarity assessment

--- server/app.py
+++ server/app.py
@@ -55,7 +55,7 @@ def predict():
         mol_error = True
 
     # checking for input - models
-    models = request.args.getlist('models')
+    models = request.args.getlist('model')
     if len(models) == 0 or models == None:
         model_error = True
 
@@ -113,7 +113,7 @@ def upload_file():
 
     response = {}
 
-    # check if the post request has the file part
+    # check if the post request has the file part, else throw error message
     if 'file' not in request.files:
         response['hasErrors'] = True
         response['errorMessages'] = 'A file needs to be attached to the request.'
@@ -121,17 +121,19 @@ def upload_file():
 
     file = request.files['file']
 
+    # check if the file has a name, else throw error message
     if file.filename == '':
         response['hasErrors'] = True
         response['errorMessages'] = 'A file with a file name needs to be attached to the request.'
         return jsonify(response)
 
+    # check if the file extension is in the allowed list of file extensions (CSV, TXT or SMI)
     if file and allowed_file(file.filename):
 
         filename = secure_filename(file.filename)
         data = dict(request.form)
         indexIdentifierColumn = int(data['indexIdentifierColumn'])
-        models = data['models'].split(';')
+        models = data['model'].split(';')
         models = [string for string in models if string != '']
         #gcnnOpt = data['gcnnOpt']
 
@@ -140,14 +142,14 @@ def upload_file():
             response['errorMessages'] = 'Please choose at least one model.'
             return jsonify(response)
 
-        if data['hasHeaderRow'] == 'true':
+        if data['hasHeaderRow'] == 'true': # file has a header row
             df = pd.read_csv(file, header=0, sep=data['columnSeparator'])
-        else:
+        else: # file does not have a header row
             df = pd.read_csv(file, header=None, sep=data['columnSeparator'])
             column_name_mapper = {}
             for column_name in df.columns.values:
                 if int(column_name) == indexIdentifierColumn:
-                    column_name_mapper[column_name] = 'mol'
+                    column_name_mapper[column_name] = 'mol' # check if this is not the case...
                 else:
                     column_name_mapper[column_name] = f'col_{column_name}'
             df.rename(columns=column_name_mapper, inplace=True)
@@ -226,8 +228,6 @@ def predict_df(df, smi_column_name, models):
     addMolsKekuleSmilesToFrame(working_df, smi_column_name)
     working_df = working_df[~working_df['mols'].isnull() & ~working_df['kekule_smiles'].isnull()]
 
-    print(f'The working df size is: {working_df.shape[0]}')
-
     if len(working_df.index) == 0:
         response['hasErrors'] = True
         response['errorMessages'] = 'We were not able to parse the smiles you provided'
@@ -255,6 +255,7 @@ def predict_df(df, smi_column_name, models):
             break
 
         pred_df = predictor.get_predictions()
+
         if data_path != '':
             predictor.record_predictions(f'{data_path}/predictions.csv')
         pred_df = working_df.join(pred_df)
@@ -284,35 +285,37 @@ def predict_df(df, smi_column_name, models):
         dict_length = len(columns_dict.keys())
         columns_dict[smi_column_name] = { 'order': 0, 'description': 'SMILES', 'isSmilesColumn': True }
 
-        if model.lower() != 'cyp450':
-            # for all models except cyp450, calculate the nearest neigbors and add additional column to response_df
-            try:
-                sim_vals = get_similar_mols(response_df[smi_column_name].values, model.lower())
-                sim_series = pd.Series(sim_vals).round(2).astype(str)
-                response_df['Tanimoto Similarity'] = sim_series.values
-                columns_dict['Tanimoto Similarity'] = { 'order': 3, 'description': 'similarity towards nearest neighbor in training data', 'isSmilesColumn': False }
-            except Exception as e:
-                app.logger.error('Error making getting similarity')
-                app.logger.error(f'error type: {type(e)}')
-                app.logger.error(e)
-        else:
-            # for cyp450 models, a similarity value is calculated using a global dataset that is representative of all six cyp450 endpoints
-            try:
-                sim_vals = get_similar_mols(response_df[smi_column_name].values, model.lower())
-                sim_series = pd.Series(sim_vals).round(2).astype(str)
-                response_df['Tanimoto Similarity'] = sim_series.values
-                columns_dict['Tanimoto Similarity'] = { 'order': 7, 'description': 'similarity towards nearest neighbor in training data that was obtained by combining the compounds from all six individual datasets', 'isSmilesColumn': False }
-            except Exception as e:
-                app.logger.error('Error making getting similarity')
-                app.logger.error(f'error type: {type(e)}')
-                app.logger.error(e)
+        if response_df.shape[0] <= 1000: # go for similarity assessment only if the response df contains 1000 compounds or less
+
+            if model.lower() != 'cyp450':
+                # for all models except cyp450, calculate the nearest neigbors and add additional column to response_df
+                try:
+                    sim_vals = get_similar_mols(response_df[smi_column_name].values, model.lower())
+                    sim_series = pd.Series(sim_vals).round(2).astype(str)
+                    response_df['Tanimoto Similarity'] = sim_series.values
+                    columns_dict['Tanimoto Similarity'] = { 'order': 3, 'description': 'similarity towards nearest neighbor in training data', 'isSmilesColumn': False }
+                except Exception as e:
+                    app.logger.error('Error calculating similarity')
+                    app.logger.error(f'error type: {type(e)}')
+                    app.logger.error(e)
+            else:
+                # for cyp450 models, a similarity value is calculated using a global dataset that is representative of all six cyp450 endpoints
+                try:
+                    sim_vals = get_similar_mols(response_df[smi_column_name].values, model.lower())
+                    sim_series = pd.Series(sim_vals).round(2).astype(str)
+                    response_df['Tanimoto Similarity'] = sim_series.values
+                    columns_dict['Tanimoto Similarity'] = { 'order': 7, 'description': 'similarity towards nearest neighbor in training data that was obtained by combining the compounds from all six individual datasets', 'isSmilesColumn': False }
+                except Exception as e:
+                    app.logger.error('Error calculating similarity')
+                    app.logger.error(f'error type: {type(e)}')
+                    app.logger.error(e)
 
         # replace SMILES with interpret SMILES when interpretation available
-        if len(model_errors) == 0:
-            if 'mol' in response_df.columns:
-                response_df[smi_column_name] = response_df['mol']
-                response_df = response_df.drop('mol', 1)
-                print(response_df.head())
+        #if len(model_errors) == 0:
+        #    if 'mol' in response_df.columns:
+        #        response_df[smi_column_name] = response_df['mol']
+        #        response_df = response_df.drop('mol', 1)
+                #print(response_df.head())
 
         response[model]['mainColumnsDict'] = columns_dict
         response[model]['data'] = response_df.replace(np.nan, '', regex=True).to_dict(orient='records')
