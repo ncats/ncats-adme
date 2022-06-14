2bcde88c340220e5fa68fb577cdc9b05cc9e9ed9
iwwwish <siramshettyv2@nih.gov>
2022-06-14 14:30:02 -0400

    typos fixed

--- client/src/app/contact/contact.component.ts
+++ client/src/app/contact/contact.component.ts
@@ -19,19 +19,19 @@ export class ContactComponent implements OnInit {
   epamImgSrc: SafeResourceUrl;
 
   constructor(
-    private domSanatizer: DomSanitizer,
+    private domSanitizer: DomSanitizer,
     @Inject(DEPLOY_URL) public deployUrl: string
   ) {
-    this.jordanImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/williamsjos.jpg`);
-    this.vishalImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/siramshettyv2.jpg`);
-    this.pranavImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/shahpa2.png`);
-    this.ewyImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/mathee.jpg`);
-    this.xinImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/xux7.jpg`);
+    this.jordanImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/williamsjos.jpg`);
+    this.vishalImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/siramshettyv2.jpg`);
+    this.pranavImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/shahpa2.png`);
+    this.ewyImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/mathee.jpg`);
+    this.xinImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/profile_images/xux7.jpg`);
 
-    this.rdkitImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/rdkit.png`);
-    this.pythonImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/python.png`);
-    this.angularImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/angular.png`);
-    this.epamImgSrc = domSanatizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/epam_ketcher.png`);
+    this.rdkitImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/rdkit.png`);
+    this.pythonImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/python.png`);
+    this.angularImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/angular.png`);
+    this.epamImgSrc = domSanitizer.bypassSecurityTrustResourceUrl(`${this.deployUrl}assets/images/epam_ketcher.png`);
   }
 
   ngOnInit(): void {
